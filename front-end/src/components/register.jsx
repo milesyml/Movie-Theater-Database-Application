2189d4ac3820d34f029d25ef3d781a797992ab46
morris88826 <mtseng9@gatech.edu>
2019-11-22 11:13:01 -0500

    register

--- front-end/src/components/register.jsx
+++ front-end/src/components/register.jsx
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
 import { Form, Button, ButtonToolbar } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 class Register extends Component {
   state = {
     username: "",
     password: ""
   };
 
-  handleChange = e => {
-    console.log("Change");
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-  };
-
-  handleSubmit = e => {
-    console.log("Submit");
-    e.preventDefault();
-    console.log(this.state);
+  btnStyle = {
+    display: "block",
+    textAlign: "center",
+    padding: 10,
+    margin: 10,
+    width: "40%",
+    marginLeft: "30%",
+    marginRight: "30%"
   };
 
   render() {
@@ -27,6 +24,15 @@ class Register extends Component {
         style={{ textAlign: "center", lineHeight: 1.6, padding: 180 }}
       >
         <h1>Register Navigation</h1>
+        <div className="btn-group">
+          <Button style={this.btnStyle}>User Only</Button>
+          <Button style={this.btnStyle}>Customer Only</Button>
+          <Button style={this.btnStyle}>Manager Only</Button>
+          <Button style={this.btnStyle}>Manager Customer</Button>
+          <Link to="/">
+            <Button style={this.btnStyle}>Back</Button>
+          </Link>
+        </div>
       </div>
     );
   }
