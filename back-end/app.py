0c4439ed96dd1f308992649609e83274b7b79030
dongsiji <dongsiji96@gmail.com>
2019-11-29 22:17:13 -0500

    Fix duplicated endpoint.

--- back-end/app.py
+++ back-end/app.py
@@ -74,20 +74,6 @@ def get_creditcards():
         msg = "Error occured: {}".format(error)
         return make_response(msg, 500)
 
-#General (Get credit cards)
-@app.route('/get_creditcards', methods=['GET'])
-def get_creditcards():
-    try:
-        if request.method == "GET":
-            cur = connection.cursor()
-            cur.execute('SELECT CreditCardNumber FROM creditcard')
-            rv = cur.fetchall()
-            cur.close()
-            return jsonify(rv)
-    except mysql.connector.Error as error:
-        msg = "Error occured: {}".format(error)
-        return make_response(msg, 500)
-
 
 #Screen 1 (User Login)
 @app.route('/user_login', methods=['POST'])
